5764d0da6d8b71af4c31aee1591e7b9dbe7d8e56
Dokukin Vyacheslav Olegovich <toorrp4@gmail.com>
2024-09-26 22:50:04 +0300

    feature(lession-9): Дописал логику добавления данных в колонку TITLE

--- lession-9/index.php
+++ lession-9/index.php
@@ -48,9 +48,13 @@ $clients = \Bitrix\Iblock\Elements\ElementClientsTable::query()
 // Добавляем заказ
 try {
     $request = Context::getCurrent()->getRequest();
+    $title = $request->getPost('TITLE');
     $compatyId = $request->getPost('COMPANY_ID');
     $clientId = $request->getPost('CLIENT_ID');
 
+    if (empty($title)) {
+        throw new \InvalidArgumentException('Не указано наименование');
+    }
     if (!intval($compatyId)) {
         throw new \InvalidArgumentException('Не указан ID компании');
     }
@@ -59,6 +63,7 @@ try {
     }
 
     $addResult  = OrderTable::add([
+        'TITLE' => $title,
         'COMPANY_ID' => $compatyId,
         'CLIENT_ID' => $clientId
     ]);
@@ -70,7 +75,7 @@ try {
     echo "<hr/><p>Добавлен заказ с ID #{$addResult->getId()}</p>";
 
 } catch (\Throwable $e) {
-    echo "<strong>{$e->getMessage()}</strong>";
+    echo "<strong style=\"color:red;\">{$e->getMessage()}</strong>";
     echo '<pre>';
     var_dump($e->getTrace());
     echo '<pre>';
@@ -83,19 +88,20 @@ $orders = OrderTable::query()
             'COMPANY_NAME' => 'COMPANY.NAME',
             'CLIENT_NAME' => 'CLIENT.NAME',
         ])
+    ->addOrder('ID', 'DESC')
     ->exec();
 ?>
 <hr/>
 <h3>Создать заказ</h3>
 <form action="" method="post">
-    <p>Наименование </p>
-    <p>Компания:<select name="COMPANY_ID">
+    <p>Наименование *: <input type="text" name="TITLE" value=""/></p>
+    <p>Компания *:<select name="COMPANY_ID">
         <?php foreach($companys as $row): ?>
             <option value="<?=$row['ID'];?>"><?=$row['NAME'];?></option>
         <?php endforeach; ?>
         </select>
     </p>
-    <p>Клиент:<select name="CLIENT_ID">
+    <p>Клиент *:<select name="CLIENT_ID">
             <?php foreach($clients as $row): ?>
                 <option value="<?=$row['ID'];?>"><?=$row['NAME'];?></option>
             <?php endforeach; ?>
@@ -110,6 +116,7 @@ $orders = OrderTable::query()
     <thead>
         <tr>
             <td>ID заказа</td>
+            <td>Наименование</td>
             <td>ID компании</td>
             <td>ID клиента</td>
             <td>Компания</td>
@@ -120,6 +127,7 @@ $orders = OrderTable::query()
         <?php foreach($orders as $order): ?>
         <tr>
             <td><?=$order['ID'];?></td>
+            <td><?=$order['TITLE'];?></td>
             <td><?=$order['COMPANY_ID'];?></td>
             <td><?=$order['CLIENT_ID'];?></td>
             <td><?=$order['COMPANY_NAME'];?></td>
