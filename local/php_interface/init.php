14c358eb0a1e09f53520105abf892a56cfc2923a
Dokukin Vyacheslav Olegovich <toorrp4@gmail.com>
2024-09-29 19:15:14 +0300

    feature(init.php): Добавил глобальный метод записи лога

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -11,3 +11,13 @@ if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
 \Bitrix\Main\Loader::registerAutoLoadClasses(null, [
     'Otus\Helpers\CBExceptionLog' => "/local/classes/Otus/Helpers/CBExceptionLog.php",
 ]);
+
+/**
+ * Сохраняет входящие данные в файл лога
+ * @param type mixed $data
+ */
+function pLog($data='',$logFileName="pLog.txt"){
+    $fp = fopen($_SERVER["DOCUMENT_ROOT"]."/local/logs/".$logFileName, "a");
+    fwrite($fp, "=================================\r\n" . date('d.m.Y H:i:s') . "\r\n" . print_r($data,true) . "\r\n");
+    fclose($fp);
+}
