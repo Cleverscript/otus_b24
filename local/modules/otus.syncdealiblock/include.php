da66fdef2a5a38e2e51996693583e2eb28234412
Dokukin Vyacheslav Olegovich <toorrp4@gmail.com>
2024-11-10 20:28:42 +0300

    feature(otus.syncdealiblock): Убрал не используемый код из include.php

--- local/modules/otus.syncdealiblock/include.php
+++ local/modules/otus.syncdealiblock/include.php
@@ -1,12 +1,2 @@
 <?php
-use Bitrix\Main\Loader;
-use Bitrix\Main\Config\Option;
 
-$module_id = "otus.syncdealiblock";
-
-$defaultOptions = Option::getDefaults($module_id);
-
-Loader::registerAutoLoadClasses(null, [
-    //'Otus\SyncDealiblock\Traits\ModuleTrait' => "/local/modules/{$module_id}/lib/Traits/ModuleTrait.php",
-    //'Otus\SyncDealiblock\Traits\HandlerTrait' => "/local/modules/{$module_id}/lib/Traits/HandlerTrait.php",
-]);
